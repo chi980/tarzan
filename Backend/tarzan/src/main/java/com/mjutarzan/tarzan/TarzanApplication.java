3fbe174ec683227c30adc2be58e80599d9125fdb
Choi mieun <104505272+chi980@users.noreply.github.com>
2024-04-12 13:53:24 +0900

    [TAR-46] config : 백엔드 초기 설정

---
+++ Backend/tarzan/src/main/java/com/mjutarzan/tarzan/TarzanApplication.java
@@ -0,0 +1,13 @@
+package com.mjutarzan.tarzan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarzanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarzanApplication.class, args);
+	}
+
+}
---
+++ Backend/tarzan/src/test/java/com/mjutarzan/tarzan/TarzanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjutarzan.tarzan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarzanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
