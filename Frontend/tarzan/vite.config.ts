cca88ac99fa0961d56ffe61fe26abfda5ac5205e
Choi mieun <104505272+chi980@users.noreply.github.com>
2024-04-21 15:55:42 +0900

    [TAR-47] config : 프론트엔드 초기 설정

--- Frontend/tarzan/vite.config.ts
+++ Frontend/tarzan/vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import styleImport from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
-    // styleImport({
-    //   preprocess: true, // SCSS 파일을 처리하도록 설정
-    // }),],
   ]
 })
