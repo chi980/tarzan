41ae8fb600e4ecabd9eb5890b40c59a39e1ac55d
Choi mieun <104505272+chi980@users.noreply.github.com>
2024-04-25 01:34:46 +0900

    [TAR-110] Design : CSS 초기화 설정

--- Frontend/tarzan/src/App.vue
+++ Frontend/tarzan/src/App.vue
@@ -1,30 +1,24 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="inner">
+      <p>러나</p>
+    </div>
+    <!-- <h2>ㅇㄹㄴ</h2> -->
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+div.container {
+  width: 100%;
+  height: 50%;
+  margin: 0;
+  padding: 0;
+  background-color: blue;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+div.inner {
+  width: 100%;
+  background-color: bisque;
 }
 </style>
