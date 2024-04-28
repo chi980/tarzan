8cd4a92357976e43c43423e4504f858d9a1af632
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-04-29 01:59:36 +0900

    Update start-tarzan.sh

--- start-tarzan.sh
+++ start-tarzan.sh
@@ -2,6 +2,6 @@
 # COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker compose -f docker-compose-prod.yml up --build -d
 # docker rmi -f $(docker images -f "dangling=true" -q) || true
 
-docker compose -f docker-compose-dec.yml pull
+docker compose -f docker-compose-dev.yml pull
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker compose -f docker-compose-dev.yml up --build -d
 docker rmi -f $(docker images -f "dangling=true" -q) || true
