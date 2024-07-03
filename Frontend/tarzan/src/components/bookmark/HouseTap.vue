f7fd293c49ba7b36d7349af2526d76841f3dd8b2
Choi mieun <104505272+chi980@users.noreply.github.com>
2024-07-03 11:21:22 +0900

    [TAR-140] feat : 집 탭 완성

--- Frontend/tarzan/src/components/bookmark/HouseTap.vue
+++ Frontend/tarzan/src/components/bookmark/HouseTap.vue
@@ -1,13 +1,110 @@
 <template>
   <div class="tab-content">
-    <h1>housetap</h1>
+    <div class="button-in-tab">
+      <p>직접 추가하기</p>
+      <img :src="HouseAddSrc" />
+    </div>
+
+    <div class="content-wrapper">
+      <div class="content-title-wrapper">
+        <p>북마크한 집 <span>0</span></p>
+        <p>
+          지도에서 집을 북마크해보세요! <br />
+          북마크한 집에 대해 여러가지를 점검할 수 있어요!
+        </p>
+      </div>
+      <div class="content">
+        <div class="non-content">
+          <p>
+            <i class="bi bi-info-circle"> </i>
+            북마크한 집이 존재하지 않습니다.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="content-indicator"></div>
+    <div class="content-wrapper">
+      <div class="content-title-wrapper">
+        <p>점검 완료한 집 <span>0</span></p>
+        <p>점검 완료한 집은 최대 3개까지 비교할 수 있어요!</p>
+      </div>
+      <div class="content">
+        <div class="non-content">
+          <p>
+            <i class="bi bi-info-circle"> </i>
+            점검 완료한 집이 존재하지 않습니다.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HouseAddSrc from "@/assets/icons/Plus/Style=Outlined.svg";
+</script>
 
 <style lang="scss" scoped>
-.tab-container {
-  background-color: beige;
+// content를 구분해주는 회색 긴 선
+.content-indicator {
+  @include custom-margin-y;
+  background-color: #ededed;
+  height: 10px;
+}
+
+// content가 없을 때 보여주는 요소
+.non-content {
+  display: flex;
+  justify-content: center; /* 가로 중앙 정렬 */
+  align-items: center; /* 세로 중앙 정렬 */
+  border-top: 1px solid #d9d9d9;
+  min-height: 140px;
+
+  p {
+    @include custom-text-description(
+      $font-color: $text-color-light,
+      $font-size: 12px
+    );
+  }
+}
+.button-in-tab {
+  @include custom-margin;
+  @include custom-button-style(
+    $bg-color: #f2f2f2,
+    $icon-size: 18px,
+    $height: 52px
+  );
+}
+// scoped
+
+.content-wrapper {
+  @include custom-padding-x;
+  display: flex;
+  flex-direction: column;
+
+  gap: $padding-default;
+
+  .content-title-wrapper {
+    text-align: left;
+    // title
+    p:first-child {
+      @include custom-text-bold($font-size: 16px);
+
+      span {
+        @include custom-text($font-color: $primary-color-default);
+      }
+    }
+    // extra desc
+    p {
+      @include custom-text-description(
+        $font-color: $text-color-light,
+        $font-size: 12px
+      );
+    }
+  }
+}
+
+.content-wrapper:last-child {
+  margin-bottom: $margin-default;
 }
 </style>
