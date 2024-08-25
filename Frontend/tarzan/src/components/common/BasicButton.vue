6f79d6262c3d364156b1136f7acb2a999f3d97d1
yerin <dpfls1022@naver.com>
2024-08-25 17:48:56 +0900

    임대인 확인 페이지 완성

---
+++ Frontend/tarzan/src/components/common/BasicButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="basic-button">
+    <button @click="navigate">{{ label }}</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'BasicButton',
+  props: {
+    to: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    navigate() {
+      this.$router.push(this.to);
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+  button {
+    width: 100%;
+    height: 48px;
+    background-color: black;
+    @include custom-text-bold($font-color: $small-button-background-color-unactive, $font-size: 14px);
+  }
+</style>
