bfde39d2dfdfc8634bbaf0a548ef86672a65d4ad
Choi mieun <104505272+chi980@users.noreply.github.com>
2024-10-06 00:18:46 +0900

    feat: d

--- Backend/tarzan/src/main/java/com/mjutarzan/tarzan/domain/board/api/request/BoardRequestDto.java
+++ Backend/tarzan/src/main/java/com/mjutarzan/tarzan/domain/board/api/request/BoardRequestDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.mjutarzan.tarzan.domain.board.model.vo.BoardTag;
 import com.mjutarzan.tarzan.global.common.vo.SiGunGu;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
@@ -21,7 +22,7 @@ public class BoardRequestDto {
     private String content;
 
     @JsonProperty("board_tag")
-    @NotBlank(message = "태그는 필수입니다.")
+    @NotNull(message = "태그는 필수입니다.")  // NotBlank 대신 NotNull 사용
     private BoardTag tag;
 
     @JsonProperty("board_gu")
