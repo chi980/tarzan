6557b5ede6dafe8c12ab5092995b630e180b6a07
Choi mieun <104505272+chi980@users.noreply.github.com>
2024-10-08 23:37:46 +0900

    feat: map api get 완성

---
+++ Backend/tarzan/src/main/java/com/mjutarzan/tarzan/domain/house/service/UserHouseServiceImpl.java
@@ -0,0 +1,15 @@
+package com.mjutarzan.tarzan.domain.house.service;
+
+import com.mjutarzan.tarzan.domain.house.repository.ApiHouseRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class HouseServiceImpl implements HouseService{
+
+}
