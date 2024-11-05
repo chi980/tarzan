8e158bfb9bb10c54555684fe77656b231ba4a89b
Choi mieun <104505272+chi980@users.noreply.github.com>
2024-11-05 21:55:02 +0900

    fix: 공인중개사 api 수정 검색조건이 먹히지 않음 RestTemplate사용

--- Backend/tarzan/src/main/java/com/mjutarzan/tarzan/domain/fraud/entity/dto/VWorldRealEstateApiResponse.java
+++ Backend/tarzan/src/main/java/com/mjutarzan/tarzan/domain/fraud/entity/dto/VWorldRealEstateApiResponse.java
@@ -3,9 +3,11 @@ package com.mjutarzan.tarzan.domain.fraud.entity.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class VWorldRealEstateApiResponse {
     @JsonProperty("EDOffices")
     private EDOffices edOffices;
