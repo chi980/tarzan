22c15a9d0413c41195cceb6e82d1287b95bc3cd8
chi980 <45choi22@gmail.com>
2025-03-29 01:58:56 +0900

    Style: 집선택 화면 수정

--- Frontend/tarzan/src/pages/SignUp.vue
+++ Frontend/tarzan/src/pages/SignUp.vue
@@ -3,7 +3,9 @@
     <form class="input-form" @submit.prevent="submitForm">
       <!-- 닉네임 입력 -->
       <div class="input-group">
-        <h2 class="input-title">닉네임<span class="input-title-mandatory">*</span></h2>
+        <h2 class="input-title">
+          닉네임<span class="input-title-mandatory">*</span>
+        </h2>
         <div class="input-content-wrapper">
           <div class="input-content">
             <input
@@ -12,17 +14,19 @@
               v-model="nickname"
               @input="handleInput"
               required
-
-      :style="{
-        border: nicknameValid === false ? '1px solid red' : '1px solid #e7e7e7',
-      }"
-            />
+              :style="{
+                border:
+                  nicknameValid === false
+                    ? '1px solid red'
+                    : '1px solid #e7e7e7',
+              }" />
           </div>
           <div class="input-description">
-
             <p v-if="isChecking">✔ 닉네임 확인 중...</p>
-            <p v-if="nicknameValid === true" >✅ 사용 가능한 닉네임입니다!</p>
-            <p v-if="nicknameValid === false" style="color:red">❌ 사용 불가능한 닉네임입니다.</p>
+            <p v-if="nicknameValid === true">✅ 사용 가능한 닉네임입니다!</p>
+            <p v-if="nicknameValid === false" style="color: red">
+              ❌ 사용 불가능한 닉네임입니다.
+            </p>
             <p>
               <i class="bi bi-info-circle"> </i>
               닉네임은 영문, 숫자로 이루어져야 합니다.
@@ -37,39 +41,54 @@
 
       <!-- 사는 곳 선택 -->
       <div class="input-group">
-        <h2 class="input-title">사는 곳<span class="input-title-mandatory">*</span></h2>  
+        <h2 class="input-title">
+          사는 곳<span class="input-title-mandatory">*</span>
+        </h2>
         <div class="select-content">
           <CustomSelectBox
             :options="seoulDistrictOptions"
-            :parentStyle="{ backgroundColor: 'white',       fontWeight: 400,
-      justifyContent: `space-between`, border: '1px solid #e7e7e7',}"
-            @update:selected="handleSeoulDistrictSelectedIdx"
-          />
+            :parentStyle="{
+              backgroundColor: 'white',
+              fontWeight: 400,
+              justifyContent: `space-between`,
+              border: '1px solid #e7e7e7',
+            }"
+            @update:selected="handleSeoulDistrictSelectedIdx" />
         </div>
       </div>
 
       <!-- 반려동물 유무 선택 -->
       <div class="input-group">
-        <h2 class="input-title">반려동물 유무<span class="input-title-mandatory">*</span></h2>
+        <h2 class="input-title">
+          반려동물 유무<span class="input-title-mandatory">*</span>
+        </h2>
         <div class="option-group">
-          <div class="option-group-item"
-           v-for="(petOption, index) in petOptions" 
-           :key="petOption.idx" 
-           :class="{ active: petOption.isSelected }"
-      @click="selectOption(petOptions, index)">{{petOption.name}}</div>
+          <div
+            class="option-group-item"
+            v-for="(petOption, index) in petOptions"
+            :key="petOption.idx"
+            :class="{ active: petOption.isSelected }"
+            @click="selectOption(petOptions, index)">
+            {{ petOption.name }}
+          </div>
         </div>
       </div>
 
       <!-- 자차 유무 선택 -->
       <div class="input-group">
-        <h2 class="input-title">자차 유무<span class="input-title-mandatory">*</span></h2>
+        <h2 class="input-title">
+          자차 유무<span class="input-title-mandatory">*</span>
+        </h2>
         <div class="option-group">
-          <div class="option-group-item"
-           v-for="(carOption, index) in carOptions" 
-           :key="carOption.idx" 
-           :class="{ active: carOption.isSelected }"
-          @click="selectOption(carOptions, index)">{{carOption.name}}</div>
-      </div>
+          <div
+            class="option-group-item"
+            v-for="(carOption, index) in carOptions"
+            :key="carOption.idx"
+            :class="{ active: carOption.isSelected }"
+            @click="selectOption(carOptions, index)">
+            {{ carOption.name }}
+          </div>
+        </div>
       </div>
 
       <!-- 학교/직장 주소 입력 -->
@@ -81,21 +100,24 @@
             placeholder="주소를 입력해주세요"
             readonly
             v-model="address"
-            @click="openAddressSearch"
-          />
+            @click="openAddressSearch" />
         </div>
       </div>
     </form>
 
     <!-- 주소 검색 팝업 -->
-    <AddressSearch
-      v-if="isAddressSearchOpen"
-      @close="closeAddressSearch" />
-    
-    <div style="width: 100%;position: absolute; bottom: 16px;display: flex; flex-direction: row;">
-      <div class="button-default" @click="submitForm">제출하기</div>
+    <AddressSearch v-if="isAddressSearchOpen" @close="closeAddressSearch" />
+
+    <div
+      style="
+        width: 100%;
+        position: absolute;
+        bottom: 16px;
+        display: flex;
+        flex-direction: row;
+      ">
+      <div class="button-default" @click="submitForm">회원가입하기</div>
     </div>
-    
   </div>
 </template>
 
@@ -139,19 +161,18 @@ const checkNicknameUnique = async (nickname: string) => {
 
   isChecking.value = true;
   try {
-      const response = await axiosInstance.post(`/v1/user/check`, {
+    const response = await axiosInstance.post(`/v1/user/check`, {
       nickname,
     });
     if (response.status === 200) {
       nicknameValid.value = true;
-  }
+    }
   } catch (error) {
     if (error.response && error.response.status === 409) {
       nicknameValid.value = false;
-    }else{
+    } else {
       console.error("닉네임 중복 체크 실패", error);
       nicknameValid.value = null;
-
     }
   } finally {
     isChecking.value = false;
@@ -209,7 +230,6 @@ const closeAddressSearch = (selectedAddress: string) => {
   isAddressSearchOpen.value = false;
 };
 
-
 /** submit form */
 const submitForm = async () => {
   try {
@@ -221,56 +241,55 @@ const submitForm = async () => {
           ? 0
           : selectedSeoulSiGunGuIdx.value
       ].value;
-      const selectedPetIdx = petOptions.value.findIndex(
-        (option) => option.isSelected
-      );
-      const selectedCarIdx = carOptions.value.findIndex(
-        (option) => option.isSelected
-      );
+    const selectedPetIdx = petOptions.value.findIndex(
+      (option) => option.isSelected
+    );
+    const selectedCarIdx = carOptions.value.findIndex(
+      (option) => option.isSelected
+    );
 
-      /** data valid한지 확인 */
-      if(nicknameValid.value == false){
-        alert("닉네임 중복을 확인해주세요");
-        return;
-      }
+    /** data valid한지 확인 */
+    if (nicknameValid.value == false) {
+      alert("닉네임 중복을 확인해주세요");
+      return;
+    }
 
-      if(gu == null){
-        alert("사는 곳을 선택해주세요");
-        return;
-      }
-      if(selectedPetIdx == -1){
-        alert("반려동물 유무를 선택해주세요");
-        return;
-      }
+    if (gu == null) {
+      alert("사는 곳을 선택해주세요");
+      return;
+    }
+    if (selectedPetIdx == -1) {
+      alert("반려동물 유무를 선택해주세요");
+      return;
+    }
 
-      if(selectedCarIdx == -1){
-        alert("자차 유무를 선택해주세요");
-        return;
-      }
+    if (selectedCarIdx == -1) {
+      alert("자차 유무를 선택해주세요");
+      return;
+    }
 
     const formData = {
       user_image_url: "https://example.com/image.jpg",
       user_nickname,
       user_gu: gu,
-      user_have_animal:
-        petOptions[selectedPetIdx].value,
-      user_have_car:
-        carOptions[selectedCarIdx].value,
+      user_have_animal: petOptions[selectedPetIdx].value,
+      user_have_car: carOptions[selectedCarIdx].value,
       user_job_address: address.value == null ? "d" : address.value,
       user_latitude: 37.5665,
       user_longitude: 126.978,
     };
 
-    const response = await axiosInstance.post("/v1/user", formData).then((response) => {
-      console.log(response.data);
-    });
+    const response = await axiosInstance
+      .post("/v1/user", formData)
+      .then((response) => {
+        console.log(response.data);
+      });
     const role = response.data.user_role;
     authStore.setRole(role);
   } catch (error) {
     console.error("회원가입 중 오류 발생", error);
   }
 };
-
 </script>
 
 <style lang="scss" scoped>
@@ -289,10 +308,10 @@ const submitForm = async () => {
   position: relative;
 }
 
-.option-group{
+.option-group {
   display: flex;
   gap: $margin-small;
-  .option-group-item{
+  .option-group-item {
     @include custom-text($font-size: 14px, $font-color: $text-color-light);
     flex: 1;
     height: 48px;
@@ -300,18 +319,17 @@ const submitForm = async () => {
     border: 1px solid $border-color-input;
     border-radius: 13px;
 
-
     display: flex;
     justify-content: center; /* 수평 중앙 정렬 */
     align-items: center; /* 수직 중앙 정렬 */
 
     transition: background-color 0.3s ease, color 0.3s ease; /* 부드러운 전환 효과 추가 */
 
-&.active {
-  background-color: $primary-color-light  ;/* active일 때 배경색 변경 */
-  color: $primary-color-default;  /* active일 때 글자색 변경 */
-  border: 1px solid $primary-color-default; /* active일 때 테두리 색 변경 */
-}
+    &.active {
+      background-color: $primary-color-light; /* active일 때 배경색 변경 */
+      color: $primary-color-default; /* active일 때 글자색 변경 */
+      border: 1px solid $primary-color-default; /* active일 때 테두리 색 변경 */
+    }
   }
 }
 
@@ -379,8 +397,11 @@ const submitForm = async () => {
   gap: $padding-default;
 }
 
-.button-default{
-  @include custom-button-style($bg-color: $secondary-color-default,$font-color: white);
+.button-default {
+  @include custom-button-style(
+    $bg-color: $secondary-color-default,
+    $font-color: white
+  );
   @include custom-margin-x;
   width: 100%;
 }
