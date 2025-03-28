9d746ddc8fbdf5cecddb7bbd89ebb37b6f7ddab0
chi980 <45choi22@gmail.com>
2025-03-29 01:58:56 +0900

    Style: 집선택 화면 수정

--- Frontend/tarzan/src/components/common/AddressSearch.vue
+++ Frontend/tarzan/src/components/common/AddressSearch.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
 /** load */
-import { ref, defineEmits } from 'vue';
+import { ref, defineEmits } from "vue";
 
 import TopBarBack from "@/components/common/TopBarBack.vue";
-import AddressSearchResult from './AddressSearchResult.vue';
-import {Building }from "@/data/Building";
+import AddressSearchResult from "./AddressSearchResult.vue";
+import { Building } from "@/data/Building";
 
 const KAKAO_API_KEY = import.meta.env.VITE_KAKAO_API_KEY; // 여기에 본인의 카카오 REST API 키를 입력하세요.
 
-const emit = defineEmits(['close']);
+const emit = defineEmits(["close"]);
 
 /** emits */
 const closeModal = () => {
-  emit('close'); // 부모 컴포넌트에 모달 닫기 이벤트 전달
+  emit("close"); // 부모 컴포넌트에 모달 닫기 이벤트 전달
 };
+
+/** */
+const selectLocation = () => {};
 
 // 검색어와 결과를 저장할 상태 변수
 const searchQuery = ref(""); // 사용자가 입력한 검색어
@@ -35,7 +38,6 @@ const selectedLocation = ref<{
   latitude: null,
   longitude: null,
 });
-
 
 // 주소 검색 함수
 const searchAddress = async () => {
@@ -71,18 +73,23 @@ const selectAddress = (selectedPlace: SearchResult) => {
   selectedLocation.value.latitude = selectedPlace.y; // 위도
   selectedLocation.value.longitude = selectedPlace.x; // 경도
 };
-
 </script>
 
 <template>
-<div class="modal-container" @click.self="closeModal">
+  <div class="modal-container" @click.self="closeModal">
     <div class="modal-wrapper">
-        <div class="modal-title" style="background-color: aqua;display: flex;flex-direction: row;"> 
-            <div style="width: 64px;height: 64px;background-color: black;" @click="closeModal"><-</div>
-            <h1>주소 검색</h1>
+      <div
+        class="modal-title"
+        style="background-color: aqua; display: flex; flex-direction: row">
+        <div
+          style="width: 64px; height: 64px; background-color: black"
+          @click="closeModal">
+          --
         </div>
-        <div class="modal-content">
-            <!-- <input type="text" v-model="searchQuery" placeholder="검색어를 입력하세요" />
+        <h1>주소 검색</h1>
+      </div>
+      <div class="modal-content">
+        <!-- <input type="text" v-model="searchQuery" placeholder="검색어를 입력하세요" />
             <div v-if="searchResults.length > 0">
                 <ul>
                     <li v-for="result in searchResults" :key="result.place_name" @click="selectAddress(result)">
@@ -91,60 +98,69 @@ const selectAddress = (selectedPlace: SearchResult) => {
                 </ul>
             </div> -->
 
-
-    <input v-model="searchQuery" type="text" placeholder="검색할 주소명을 입력해주세요" class="search-input"/>
-<AddressSearchResult></AddressSearchResult>
-        </div>
+        <input
+          v-model="searchQuery"
+          type="text"
+          inputmode="text"
+          enterkeyhint="enter"
+          placeholder="검색할 주소명을 입력해주세요"
+          class="search-input" />
+        <AddressSearchResult></AddressSearchResult>
+      </div>
     </div>
 
     <div class="button-wrapper">
-        <button class="button-default" @click="closeModal">검색</button>
+      <button class="button-default" @click="selectLocation">선택</button>
     </div>
-</div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-.modal-wrapper{
+.modal-wrapper {
+  display: flex;
+  flex-direction: column;
+
+  height: 100%;
+
+  .modal-title {
+    height: $height-top-bar;
+    background-color: aqua;
+  }
+
+  .modal-content {
+    @include custom-padding-x($padding-default);
+    @include custom-padding-y($padding-big);
+
     display: flex;
     flex-direction: column;
 
     height: 100%;
-
-    .modal-title{
-        height: $height-top-bar;
-        background-color: aqua;
-    }
-
-    .modal-content{
-
-@include custom-padding-x($padding-default);
-@include custom-padding-y($padding-big);
-
-display: flex;
-flex-direction: column;
-
-height: 100%;;
-overflow-y: auto; /* 세로 스크롤을 추가 */
-@include custom-scrollbar-style; /* 스크롤바 스타일 적용 */
-
-    
+    overflow-y: auto; /* 세로 스크롤을 추가 */
+    @include custom-scrollbar-style; /* 스크롤바 스타일 적용 */
+  }
 }
-}
-.modal-container{
-    @include custom-modal;
+.modal-container {
+  @include custom-modal;
 }
 
-.search-input{
-    @include custom-input-style;
-    // @include custom-shadow-style;
+.search-input {
+  @include custom-input-style;
+  // @include custom-shadow-style;
 }
 
-.button-wrapper{
-    width: 100%;position: absolute; bottom: $padding-default;display: flex; flex-direction: row;
+.button-wrapper {
+  width: 100%;
+  position: absolute;
+  bottom: $padding-default;
+  display: flex;
+  flex-direction: row;
 }
 
-.button-default{
-  @include custom-button-style($bg-color: $secondary-color-default,$font-color: white);
+.button-default {
+  @include custom-button-style(
+    $bg-color: $secondary-color-default,
+    $font-color: white
+  );
   @include custom-margin-x;
 }
 </style>
