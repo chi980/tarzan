5572529af823689ee7a170254b03888bf718ae43
chi980 <45choi22@gmail.com>
2025-04-15 23:08:08 +0900

    Refactor: 사용자 response dto 변경 * role 포함

--- Backend/tarzan/src/main/java/com/mjutarzan/tarzan/domain/bookmark/api/response/CompareBookmarkDetailResponseDto.java
+++ Backend/tarzan/src/main/java/com/mjutarzan/tarzan/domain/bookmark/api/response/CompareBookmarkDetailResponseDto.java
@@ -7,14 +7,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Getter
 @NoArgsConstructor
 public class CompareBookmarkDetailResponseDto {
+    @JsonProperty("bookmark_id")
+    private Long id;
 
     @JsonProperty("house_id")
-    private Long id;
+    private Long houseId;
 
     @JsonProperty("house_name")
     private String name;
@@ -28,20 +32,33 @@ public class CompareBookmarkDetailResponseDto {
     @JsonProperty("house_score")
     private Integer score;
 
+    @JsonProperty("house_costs")
+    Map<String, String> costs;
+    @JsonProperty("house_details")
+    Map<String, String> details;
+
     @JsonProperty("house_indexes")
     Map<HouseIndexType, Long> indexes;
 
     @JsonProperty("house_checks")
     Map<BookmarkChecklistType, Long> checks;
 
+
+    @JsonProperty("bookmark_created_at")
+    private LocalDateTime bookmarkCreatedAt;
+
     @Builder
-    public CompareBookmarkDetailResponseDto(Long id, String name, String address, String category, Integer score, Map<HouseIndexType, Long> indexes, Map<BookmarkChecklistType, Long> checks) {
+    public CompareBookmarkDetailResponseDto(Long id, Long houseId, String name, String address, String category, Integer score, Map<String, String> costs, Map<String, String> details, Map<HouseIndexType, Long> indexes, Map<BookmarkChecklistType, Long> checks, LocalDateTime bookmarkCreatedAt) {
         this.id = id;
+        this.houseId = houseId;
         this.name = name;
         this.address = address;
         this.category = category;
         this.score = score;
+        this.costs = costs;
+        this.details = details;
         this.indexes = indexes;
         this.checks = checks;
+        this.bookmarkCreatedAt = bookmarkCreatedAt;
     }
 }
