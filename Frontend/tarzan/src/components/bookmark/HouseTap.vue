cfc817f19e137dcd2413774a7e8086e0b79f6c2d
chi980 <45choi22@gmail.com>
2025-04-26 00:13:20 +0900

    Refactor: isNext적용

--- Frontend/tarzan/src/components/bookmark/HouseTap.vue
+++ Frontend/tarzan/src/components/bookmark/HouseTap.vue
@@ -133,14 +133,23 @@ const fetchBookmarks = async (page: number, params: any) => {
     });
 
     if (response.data.success) {
-      return response.data.data.list;
+      return {
+        items: response.data.data.list,
+        isNext: response.data.data.isNext,
+      };
     } else {
       console.error("Failed to fetch data:", response.data.message);
-      return [];
+      return {
+        items: [],
+        isNext: false,
+      };
     }
   } catch (error) {
     console.error("API request error:", error);
-    return [];
+    return {
+      items: [],
+      isNext: false,
+    };
   }
 };
 </script>
