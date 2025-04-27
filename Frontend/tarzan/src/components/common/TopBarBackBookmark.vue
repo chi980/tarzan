6688b40c7beb5a2a767f3b50048f8618542b99ba
yeeunp <yeeunpb@gmail.com>
2025-04-27 19:38:21 +0900

    feat: 북마크 자잘한 수정사항 완

---
+++ Frontend/tarzan/src/components/common/TopBarBackBookmark.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="topbar">
+    <img id="back-button"
+         src="@/assets/icons/Filter/back-icon.png" 
+         alt="back-button"
+         @click="goToBookmark">
+    <div class="title">{{ title }}</div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Topbar',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    goToBookmark() {
+    this.$router.push({ name: 'BookMark' }).then(() => {
+        window.location.reload();
+    });
+    }
+
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.topbar {
+  background-color: white;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+  @include custom-text($font-weight: 800, $font-size: 16px);
+  @include custom-padding-x;
+  box-sizing: border-box;
+}
+
+.title {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 18px;
+  font-weight: bold;
+}
+
+#back-button {
+  width: 16px;
+}
+</style>
