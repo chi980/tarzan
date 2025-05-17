0089869eea75cdd595c6da0587739969a1f4406b
chi980 <45choi22@gmail.com>
2025-05-17 20:12:10 +0900

    Style: 직접 추가하기 화면 수정 * 상단바, 하단바 모두 컴포넌트로 대치 * css 수정

--- Frontend/tarzan/src/components/bookmark/AddHousePage.vue
+++ Frontend/tarzan/src/components/bookmark/AddHousePage.vue
@@ -1,44 +1,49 @@
 <template>
   <div class="sub-container non-input-sub-container">
-    <AddHouseBar class="add-house-bar" />
+    <div class="top-bar-wrapper">
+      <TopBarBack :title="''" @back="backToBookmark" />
+    </div>
     <div class="center-container">
-      <form class="input-form">
+      <form class="input-form" @submit.prevent="">
         <div class="input-group">
-          <h2 class="input-title">건물 종류</h2>
-            <div class="select-wrapper">
-              <CustomSelectBox
-                :options="HouseCategoryOptions"
-                :parentStyle="{
-                  backgroundColor: 'white',
-                  fontWeight: 400,
-                  justifyContent: `space-between`,
-                  border: '1px solid #e7e7e7',
-                }"
-                @update:selected="handleBuildingCategorySelected"
-              />
-            </div>
+          <h2 class="input-title">
+            건물 종류<span class="input-title-mandatory">*</span>
+          </h2>
+          <div class="select-content">
+            <CustomSelectBox
+              :options="HouseCategoryOptions"
+              :parentStyle="{
+                backgroundColor: 'white',
+                fontWeight: 400,
+                justifyContent: `space-between`,
+                border: '1px solid #e7e7e7',
+              }"
+              @update:selected="handleBuildingCategorySelected" />
           </div>
+        </div>
 
         <div class="input-group">
-          <h2 class="input-title">건물 이름</h2>
-          <div class="input-content" :class="{'error': !house_name && showError}">
+          <h2 class="input-title">
+            건물 이름<span class="input-title-mandatory">*</span>
+          </h2>
+          <div
+            class="input-content"
+            :class="{ error: !house_name && showError }">
             <input
               type="text"
               v-model="house_name"
-              placeholder="건물명을 입력해주세요"
-            />
+              placeholder="건물명을 입력해주세요" />
           </div>
-        <div v-if="!house_name || !house_name.trim() || showError" class="input-description error-message">
-          <p>
-            <i class="bi bi-info-circle"></i>
-            건물 이름은 필수 정보입니다.
-          </p>
-        </div>
         </div>
         <div class="input-group">
-          <h2 class="input-title">주소</h2>
+          <h2 class="input-title">
+            주소<span class="input-title-mandatory">*</span>
+          </h2>
           <div class="input-content">
-            <input type="text" v-model="house_address" placeholder="주소를 입력해주세요" />
+            <input
+              type="text"
+              v-model="house_address"
+              placeholder="주소를 입력해주세요" />
           </div>
           <div class="input-description">
             <p>
@@ -48,27 +53,25 @@
           </div>
         </div>
       </form>
-      <div class="bottom-button-wrapper">
-        <div @click="handleAddHouseClick">
-          <p>직접 추가하기</p>
-        </div>
-      </div>
     </div>
+    <BottomDefaultButton :label="'추가하기'" :onClick="handleAddHouseClick" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
-import { useRoute, useRouter } from 'vue-router';
+import { useRoute, useRouter } from "vue-router";
 import { axiosInstance } from "@/plugins/axiosPlugin";
-import AddHouseBar from "@/components/bookmark/AddHouseBar.vue";
-import CustomSelectBox from '@/components/common/CustomSelectBox.vue';
+
+import TopBarBack from "@/components/common/TopBarBack.vue";
+import CustomSelectBox from "@/components/common/CustomSelectBox.vue";
+import BottomDefaultButton from "@/components/common/BottomDefaultButton.vue";
 
 // const searchQuery = ref(""); 검색기능 사용시
 
-const showError = ref(false);  // 오류 메시지 표시 여부
-const route = useRoute();      // useRoute: 현재 URL 정보
-const router = useRouter();    // useRouter: 페이지 이동
+const showError = ref(false); // 오류 메시지 표시 여부
+const route = useRoute(); // useRoute: 현재 URL 정보
+const router = useRouter(); // useRouter: 페이지 이동
 
 const house_address = ref("");
 const house_name = ref("");
@@ -80,10 +83,10 @@ const list = ref([]); // ✅ 추가한 집 목록을 저장
 
 // 건물 종류 선택지를 배열로 정의
 const HouseCategoryOptions = [
-  { idx: 0, name: '아파트', value: '아파트' },
-  { idx: 1, name: '오피스텔', value: '오피스텔' },
-  { idx: 2, name: '빌라', value: '빌라' },
-  { idx: 3, name: '주택', value: '주택' }
+  { idx: 0, name: "아파트", value: "아파트" },
+  { idx: 1, name: "오피스텔", value: "오피스텔" },
+  { idx: 2, name: "빌라", value: "빌라" },
+  { idx: 3, name: "주택", value: "주택" },
 ];
 
 // 선택된 건물 종류의 인덱스를 저장할 변수
@@ -92,13 +95,13 @@ const selectedBuildingCategoryIdx = ref(null);
 // 선택된 건물 종류를 처리하는 메서드
 const handleBuildingCategorySelected = (idx: number) => {
   selectedBuildingCategoryIdx.value = idx;
-  house_category.value = HouseCategoryOptions[idx].value;  // house_category 값 업데이트
-  console.log('선택된 건물 종류:', HouseCategoryOptions[idx].name);
+  house_category.value = HouseCategoryOptions[idx].value; // house_category 값 업데이트
+  console.log("선택된 건물 종류:", HouseCategoryOptions[idx].name);
 };
 
 onMounted(() => {
-  house_address.value = route.query.house_address as string || '';
-  house_name.value = route.query.house_name as string || '';
+  house_address.value = (route.query.house_address as string) || "";
+  house_name.value = (route.query.house_name as string) || "";
   house_latitude.value = route.query.house_latitude
     ? parseFloat(route.query.house_latitude as string)
     : null;
@@ -109,12 +112,13 @@ onMounted(() => {
 
 // "직접 추가하기" 버튼 클릭 시
 const handleAddHouseClick = async () => {
-  if (!house_name.value || !house_name.value.trim()) {  // 🚨 빈 값과 공백 체크
+  if (!house_name.value || !house_name.value.trim()) {
+    // 🚨 빈 값과 공백 체크
     showError.value = true;
     return;
   }
 
-  showError.value = false;  // 정상 입력 시 에러 메시지 숨김
+  showError.value = false; // 정상 입력 시 에러 메시지 숨김
 
   const newHouse = {
     house_name: house_name.value.trim(),
@@ -128,9 +132,11 @@ const handleAddHouseClick = async () => {
     const response = await axiosInstance.post("/v1/bookmark/user", newHouse);
     list.value.push(newHouse);
 
-    router.push({ name: "BookMark", query: { list: JSON.stringify(list.value) } }).then(() => {
-      window.location.reload();
-    });
+    router
+      .push({ name: "BookMark", query: { list: JSON.stringify(list.value) } })
+      .then(() => {
+        window.location.reload();
+      });
 
     console.log("Response:", response.data);
   } catch (error) {
@@ -138,25 +144,20 @@ const handleAddHouseClick = async () => {
   }
 };
 
-
-
-
-
 onMounted(() => {
-  house_address.value = route.query.house_address as string || '';
-  house_name.value = route.query.house_name as string || '';
+  house_address.value = (route.query.house_address as string) || "";
+  house_name.value = (route.query.house_name as string) || "";
 });
+
+const backToBookmark = () => {
+  router.push({ name: "BookMark" });
+};
 </script>
 
 <style lang="scss" scoped>
 // 공통
-.top-bar-back {
-  @include custom-bar-style(
-    $height: $height-top-bar,
-    $z-index: $z-index-top-bar
-  );
-
-  background-color: aqua;
+.top-bar-wrapper {
+  width: 100%;
 }
 
 .sub-container {
@@ -164,80 +165,92 @@ onMounted(() => {
   flex-direction: column;
   width: 100%;
 }
-
 .center-container {
+  position: relative;
   flex-grow: 1;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  overflow-y: auto;
+  /* 스크롤바 전체 영역 */
+  &::-webkit-scrollbar {
+    width: 4px; /* 세로축 스크롤바 폭 너비 */
+    height: 100%; /* 가로축 스크롤바 폭 너비 */
+  }
+  &::-webkit-scrollbar-button {
+    display: none;
+  }
+  /* 스크롤바 막대 제외 부분 */
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+  /* 스크롤바 막대 */
+  &::-webkit-scrollbar-thumb {
+    border-radius: calc($border-radius-default * 2);
+    background: #f2f2f2;
+  }
+}
+
+.input-form {
   width: 100%;
   display: flex;
   flex-direction: column;
-  overflow-y: auto;
-  margin-top: 20px;
-}
-
-.input-group:first-child {
-  margin-top: 0;
-}
-
-.input-group {
-  @include custom-padding-x;
-  margin-top: $margin-big;
-
-  .input-title {
-    text-align: left;
-    padding-bottom: 10px;
-    font-weight: bold;
-  }
-
-  .input-content {
+  gap: $padding-big;
+  .input-group {
+    @include custom-padding-x();
     display: flex;
-    align-items: center;
-    gap: $padding-default;
+    flex-direction: column;
+    gap: 12px;
 
-    input[type="text"] {
-      @include custom-input-style;
-      flex: 1;
+    .input-title {
+      @include custom-text($font-size: 14px);
+      text-align: left;
+
+      .input-title-mandatory {
+        color: red;
+      }
     }
 
-    .select-wrapper {
-      @include custom-input-style;
-      width: 100%; // select 박스가 꽉 차게
+    .input-content-wrapper {
+      display: flex;
+      flex-direction: column;
+      gap: $padding-small;
+
+      .input-content {
+        margin: 0;
+      }
+    }
+
+    .input-content {
       display: flex;
       align-items: center;
-      justify-content: space-between;
-      cursor: pointer; // 클릭 가능한 느낌
+      gap: $padding-small;
+
+      input[type="text"] {
+        @include custom-input-style;
+        flex-grow: 1; /* 남은 공간을 모두 차지하도록 설정 */
+      }
+
+      .select-content {
+        flex-grow: 1;
+      }
     }
-  }
+    .input-description {
+      @include custom-text($font-size: 12px, $font-color: $text-color-light);
 
-
-  .input-description {
-    @include custom-text($font-size: 12px, $font-color: $text-color-light);
-
-    p {
-      display: block;
-      @include custom-padding-y(4px);
-      text-align: left;
+      p {
+        display: block;
+        @include custom-padding-y(4px);
+        text-align: left;
+      }
     }
-  }
-
-  .error {
-    border: 1px solid red;  /* 빨간색 테두리 */
-    border-radius: 14px;  /* 테두리의 radius 조정 */
-  }
-
-  .error-message {
-    color: red;  /* 빨간색 텍스트 */
   }
 }
 
-.bottom-button-wrapper {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  @include custom-padding-x;
-
-  div {
-    @include custom-button-style($height: 54px, $font-color: white);
-  }
+// scoped
+.center-container {
+  justify-content: center;
 }
 </style>
