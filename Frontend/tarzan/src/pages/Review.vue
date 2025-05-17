4bd846eb65b55d8847e74a43421bf5f8400f4014
chi980 <45choi22@gmail.com>
2025-05-17 19:29:06 +0900

    Fix: 리뷰 요약 api 연결 수정 * 받아온 결과를 model에 대입

--- Frontend/tarzan/src/pages/Review.vue
+++ Frontend/tarzan/src/pages/Review.vue
@@ -78,14 +78,20 @@ const reviewSummary = ref({
   review_count: 0,
 });
 const fetchReviewSummary = async (houseIdx: number) => {
+  console.log("houseIdx:", houseIdx);
   try {
     const response = await axiosInstance.get(`/v1/reviews/summary`, {
-      params: {
-        house_id: houseIdx,
-      },
+      params: { houseId: houseIdx },
     });
-
-    console.log(response.data);
+    if (response.data.success) {
+      console.log("리뷰 요약:", response.data);
+      reviewSummary.value = {
+        review_score: response.data.data.review_score,
+        review_count: response.data.data.review_count,
+      };
+    } else {
+      throw new Error("Failed to fetch review summary");
+    }
   } catch (error) {
     console.error("API request error:", error);
   }
